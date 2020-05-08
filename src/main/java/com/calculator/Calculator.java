695c77a90e0ce19a66d623bb91b380435d7df161
ana1008 <chinks.ananya123@gmail.com>
2020-05-08 20:17:52 +0530

    Update Calculator.java

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public static void main(String[] args){
 		System.out.println("6 + 3 = " + add(6, 3));
 		System.out.println("6 - 3 = " + subtract(6,3));
-//		System.out.println("6 * 3 = " + multiply(6,3));
+		System.out.println("6 * 3 = " + multiply(6,3));
 //		System.out.println("6 / 3 = " + divide(6,3));
 	}
 
@@ -17,9 +17,9 @@ public class Calculator {
 		return a - b;
 	}
 //
-//	public static long multiply(int a, int b) {
-//		return a * b;
-//	}
+	public static long multiply(int a, int b) {
+		return a * b;
+	}
 //
 //	public static double divide(int a, int b) {
 //		double result;
